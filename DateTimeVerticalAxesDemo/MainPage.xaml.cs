1b14b55bc956341ab611535f007de1836f95f9de
Naveenkumar S <naveenkumar.sanjeevirayan@syncfusion.com>
2022-11-11 19:28:33 +0530

    updated sample codes

--- DateTimeVerticalAxesDemo/MainPage.xaml.cs
+++ DateTimeVerticalAxesDemo/MainPage.xaml.cs
@@ -10,7 +10,10 @@ public partial class MainPage : ContentPage
 	private void OnNumericalAxisLabelCreated(object sender, Syncfusion.Maui.Charts.ChartAxisLabelEventArgs e)
 	{
 		double value = Convert.ToDouble(e.Label);
+		// Converting double value to its respective date time value.
 		var date = DateTime.FromOADate(value);
+
+		// formatting date time string to display on the y-axis
 		e.Label = String.Format("{0:hh:mm tt }", date);
 	}
 }
